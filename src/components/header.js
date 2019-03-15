da4a0fa0263ff0f5252e108b0244146bbb5a7801
byronhsu <byronhsu1230@gmail.com>
2019-03-15 18:41:12 +0800

    first commit

---
+++ src/components/header.js
@@ -0,0 +1,33 @@
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React from "react"
+
+const Header = () => (
+  <header
+    style={{
+      background: `#0f2345`,
+      marginBottom: `1.45rem`,
+    }}
+  >
+    <div
+      style={{
+        maxWidth: 960,
+        padding: `1.5rem 1.0875rem`,
+      }}
+    >
+      <h1 style={{ margin: 0 }}>
+        <Link
+          to="/"
+          style={{
+            color: `white`,
+            textDecoration: `none`,
+          }}
+        >
+          NTUEE<span style={{color: '#00bfdf'}}>+</span>
+        </Link>
+      </h1>
+    </div>
+  </header>
+)
+
+export default Header
