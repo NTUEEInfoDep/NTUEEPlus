0f4f3c557d4611f3776270dd94e7959df5515d2b
tzyLee <b06901145@ntu.edu.tw>
2019-03-16 22:00:39 +0800

    Add tag to the top of article

--- gatsby-node.js
+++ gatsby-node.js
@@ -17,13 +17,14 @@ exports.createPages = ({ graphql, actions }) => {
             htmlAst
             frontmatter {
               path
+              tags
             }
           }
         }
       }
     }
   `).then(result => {
-    // 用graphql拿到所有markdown的path以及html Abstract syntax tree
+    // 用graphql拿到所有markdown的path以及html Abstract syntax tree（相當於markdown的內容轉換成html）
     if (result.errors) {
       throw result.errors; // 如果出錯直接丟出Error
     }
@@ -33,7 +34,8 @@ exports.createPages = ({ graphql, actions }) => {
         path: edge.node.frontmatter.path, // 把markdown host在markdown自己提供的path上（沒有檢查重複）（應該）
         component: path.resolve('src/templates/article.jsx'), // 用來處理markdown的Component
         context: {
-          htmlAst: edge.node.htmlAst
+          htmlAst: edge.node.htmlAst,
+          tags: edge.node.frontmatter.tags
           // 要傳給components的prop，可以用來當graphql的argument
         }
       });
--- src/templates/article.jsx
+++ src/templates/article.jsx
@@ -18,6 +18,11 @@ const renderAst = new rehypeReact({
 export default props => (
   <React.Fragment>
     <Header />
+    <div className={style.tags}>
+      {props.pathContext.tags.map((tag, index) => (
+        <span key={index}>{tag}</span> // tag是寫好markdown就不變的，照理說不會因為state而有所改變，所以用index當key沒差
+      ))}
+    </div>
     <article className={style.article}>
       {renderAst(props.pathContext.htmlAst)}
       {/*pathContext: 在gatsby-node.js的constext */}
